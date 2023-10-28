2b071ba289a320d226cae64b9e79177c0a85dbab
ChelseyBommer <chelsey.bommer@hva.nl>
2023-10-28 13:27:52 +0200

    change: menu button toggle
    Button now moves the menu items

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+/* Variabele aanmaken */
+var Button = document.querySelector("nav button")
+
+Button.addEventListener("click", Menu);
+
+function Menu () {
+    var MenuLijst = document.querySelector("nav ul");
+    MenuLijst.classList.toggle("MenuAdd");
+}
